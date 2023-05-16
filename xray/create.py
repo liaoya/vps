7b13ba112f592cb1c07a31cc090831fae9b0506c
Huifeng Shen <liaoya@gmail.com>
2023-05-16 20:50:31 +0800

    Add python file

---
+++ xray/create.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import subprocess
+import shlex
+
+from typing import Any, Mapping, Optional
+
+def create_kcp_option()->Mapping[str, Any]:
+    mm = {}
+    return mm
+
+def create_shadowsocks_option()->Mapping[str, Any]:
+    mm = {}
+    return mm
+
+def write_option_file(filename: str, mm: Mapping[str, Any]):
+    ll = sorted(mm.keys())
+    with open(filename, "w") as fp:
+        for key in ll:
+            print(f"{key}={mm[key]}", file=fp)
+
+def main(filename: str, mode: str, operation: str, protocal: str, stream: Optional[str]):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create Xray config file and start docker-compose',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-f", "--filename", help="option file name")
+    parser.add_argument("-m", "--mode", choices=("client", "server"), required=True, help="")
+    parser.add_argument("-o", "--operation", choices=("clean", "restart", "start", "stop"), required=True, help="")
+    parser.add_argument("-p", "--protocol", choices=("shadowsocks", "vmess"), required=True, help="")
+    parser.add_argument("-s", "--stream", choices=("kcp",), help="")
+    parser.add_argument("-v", "-verbose", action='count', default=0, help="")
+    args = parser.parse_args()
+    main()
